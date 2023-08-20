6f17a858af21acc887312354bfdd200a2a441a20
raulG91 <rgarciapedrosa@gmail.com>
2023-08-20 16:35:04 +0200

    Prod configuration

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,7 @@ from .users import user
 from .private import db
 def create_app():
      app = Flask(__name__)
-     '''app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
-     app.config['UPLOAD_FOLDER'] = 'app/private/static/images'
-     '''
-     app.config.from_object("config.DevConfig")
+     app.config.from_object("config.ProdConfig")
      print(app.config['HOST'])
      #Import BluePrint for public part
      from .public import public_bp
