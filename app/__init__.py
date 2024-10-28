292c62851e6285b2355433359ec0583dfe24b635
raulG91 <=>
2024-10-28 22:59:35 +0100

    Fixing rotation in pictures v2

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ def create_app():
      app = Flask(__name__)
 
 
-     app.config.from_object("config.DevConfig")
+     app.config.from_object("config.ProdConfig")
      path = Path(__file__).parent.resolve()
      app.config["UPLOAD_FOLDER"] = str(path) + app.config["UPLOAD_FOLDER"]
      #Import BluePrint for public part
